c43fb1692da089283a94db3578a132e85c9db38e
soufiane amabraa <sfn@MacBook-Pro-de-mac.local>
2020-09-07 16:41:59 +0100

    dat é

--- starter/app.js
+++ starter/app.js
@@ -15,8 +15,50 @@ score = [0, 0];
 roundScore = [0, 0];
 activePlayer = 0;
 
-document.querySelector('#score-0').textContent = score[0];
+function newG() {
+  score = [0, 0];
+  roundScore = [0, 0];
+  activePlayer = 0;
+  document.querySelector('#score-0').textContent = score[0];
+  document.querySelector('#score-1').textContent = score[1];
+  document.querySelector('.dice').style.display = 'none';
+  document.querySelector('#current-0').textContent = roundScore[0];
+  document.querySelector('#current-1').textContent = roundScore[1];
+}
+newG();
+function rScoreUP() {
+  document.querySelector(`#current-${activePlayer}`).textContent =
+    roundScore[activePlayer];
+}
 
-document.querySelector('#score-1').textContent = score[1];
+function scoreUP() {
+  document.querySelector(`#score-${activePlayer}`).textContent =
+    score[activePlayer];
+}
 
-document.querySelector('.dice').style.display = 'none';
+document.querySelector('.btn-new').addEventListener('click', newG);
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+  const dice = Math.floor(Math.random() * 6) + 1;
+  const diceDOM = document.querySelector('.dice');
+  if (dice > 1) {
+    roundScore[activePlayer] = roundScore[activePlayer] + dice;
+    rScoreUP();
+    diceDOM.style.display = 'inline';
+    diceDOM.src = `dice-${dice}.png`;
+  } else {
+    roundScore[activePlayer] = 0;
+    rScoreUP();
+    diceDOM.style.display = 'inline';
+    diceDOM.src = `dice-${dice}.png`;
+    activePlayer = activePlayer > 0 ? 0 : 1;
+  }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', () => {
+  score[activePlayer] = score[activePlayer] + roundScore[activePlayer];
+  scoreUP();
+  roundScore[activePlayer] = 0;
+  rScoreUP();
+  activePlayer = activePlayer > 0 ? 0 : 1;
+});
